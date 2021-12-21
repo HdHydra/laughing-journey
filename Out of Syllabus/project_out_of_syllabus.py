a43754c8e7be590b21f8f99808edbaef689d790b
HdHydra <51809407+HdHydra@users.noreply.github.com>
2021-12-21 17:40:45 +0530

    Update project_out_of_syllabus.py

--- Out of Syllabus/project_out_of_syllabus.py
+++ Out of Syllabus/project_out_of_syllabus.py
@@ -1,5 +1,5 @@
 #python 3.6.9
-#This project is built by Riwader Nabhan 
+#This project is built by Froza
 #N A for Out Of Syllabus python course 
 
 from datetime import datetime as date
@@ -7,7 +7,7 @@ import time
 
 today = date.now()
 
-print("Project developed by Riwader Nabhan NSS Engineering college")
+print("Project developed by Froza from Freezing College")
 print ("program executed at ", today.strftime("%c"))
 
 
